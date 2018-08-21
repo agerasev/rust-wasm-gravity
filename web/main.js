85b341a2c88e2288bd7bd1592ccdf72520ba554e
Alexey Gerasev <alexey.gerasev@gmail.com>
2018-08-21 12:00:27 +0700

    update string load from wasm

--- web/main.js
+++ web/main.js
@@ -1,10 +1,19 @@
 let wasm = null;
 
+let load_str = (ptr, len) => {
+    const view = new Uint8Array(wasm.exports.memory.buffer, ptr, len);
+    //const utf8dec = new TextDecoder("utf-8");
+    //return utf8dec.decode(view);
+    let str = "";
+    for (let i = 0; i < view.length; i++) {
+        str += String.fromCharCode(view[i]);
+    }
+    return str;
+}
+
 let env = {
     js_console: (type, ptr, len) => {
-        const view = new Uint8Array(wasm.exports.memory.buffer, ptr, len);
-        const utf8dec = new TextDecoder("utf-8");
-        const str = utf8dec.decode(view);
+        let str = load_str(ptr, len);
         if (type == 1) {
             console.error(str);
         } else {
@@ -15,7 +24,7 @@ let env = {
 
 let import_env = (env, im_env, prefix) => {
     prefix = !prefix ? "" : prefix;
-    for (var key in im_env) {
+    for (let key in im_env) {
         if (im_env.hasOwnProperty(key)) {
             env[prefix + key] = im_env[key];
         }
