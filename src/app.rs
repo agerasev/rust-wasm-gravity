a8e5156d7764c566f5a126ba7a02184581b2a9ef
Alexey Gerasev <alexey.gerasev@gmail.com>
2018-08-27 11:11:02 +0700

    physics traits and functions

--- src/app.rs
+++ src/app.rs
@@ -1,45 +1,103 @@
-use std::f64::consts::PI;
-
 use vecmat::vec::*;
 
 use console;
-use math::{cos};
+use math::{pow};
 use canvas::{Canvas, Color, Path, Method};
+use physics::*;
+
+pub struct Body {
+    var: Point2,
+    mass: f64,
+    color: Color,
+}
+
+impl<'a> Variable<'a> for Body {
+    type VarIter = <Point2 as Variable<'a>>::VarIter;
+    type VarState = Point2;
+    fn var_iter(&'a mut self) -> Self::VarIter {
+        self.var.var_iter()
+    }
+    fn var_clone(&'a self) -> Self::VarState {
+        self.var.var_clone()
+    }
+}
+
+pub struct System {
+    bodies: Vec<Body>,
+    deriv: Vec<Point2>,
+    buf0: Vec<Point2>,
+    buf1: Vec<Point2>,
+}
+
+impl System {
+    fn new(bodies: Vec<Body>) -> System {
+        let deriv = bodies.var_clone();
+        let (buf0, buf1) = (bodies.var_clone(), bodies.var_clone());
+        System { bodies, deriv, buf0, buf1 }
+    }
+}
 
 pub struct App {
     time: f64,
     canvas: Canvas,
-    //system: System,
+    system: System,
 }
 
 impl App {
     pub fn new() -> Self {
-        /*
-        let mut system = System { bodies: vec!(
-            Point { pos: Vec2f64::from_arr([200.0, 0.0]), vel: Vec2f64::from_arr([0.0, 10.0]) },
-            Point { pos: Vec2f64::from_arr([400.0, 0.0]), vel: Vec2f64::from_arr([0.0,-10.0]) },
-        ) };
-        */
+        let system = System::new(vec!(
+            Body { 
+                var: Point2 { pos: Vec2f64::from_arr([200.0, 300.0]), vel: Vec2f64::from_arr([0.0, 100.0]) },
+                mass: 10.0, color: Color::from_arr([1.0, 0.0, 0.0, 1.0]),
+            },
+            Body { 
+                var: Point2 { pos: Vec2f64::from_arr([600.0, 300.0]), vel: Vec2f64::from_arr([0.0,-100.0]) },
+                mass: 10.0, color: Color::from_arr([0.0, 0.0, 1.0, 1.0]),
+            },
+        ));
         my_print!("App created!");
-        App { time: 0.0, canvas: Canvas::new() }//, system }
+        App { time: 0.0, canvas: Canvas::new(), system }
+    }
+
+    pub fn gravity(bodies: &Vec<Body>, deriv: &mut Vec<Point2>) {
+        for (i, (b, db)) in bodies.iter().zip(deriv.iter_mut()).enumerate() {
+            db.pos = b.var.vel;
+            db.vel = Vec2f64::zero();
+            for (j, bo) in bodies.iter().enumerate() {
+                if i != j {
+                    let r = bo.var.pos - b.var.pos;
+                    let g = r/(pow(r.sqr(), 1.5));
+                    db.vel += g*2e7;
+                }
+            }
+        }
     }
 
     pub fn step(&mut self, dt: f64) {
         self.time += dt;
+
+        let s = &mut self.system;
+
+        solve_rk4(
+            &mut s.bodies,
+            &mut s.deriv,
+            &mut s.buf0,
+            &mut s.buf1,
+            dt,
+            Self::gravity
+        );
     }
 
     pub fn draw(&mut self) {
-        self.canvas.clear();
-        let mut rad = Vec2f64::from_scal(1.5) + Vec2f64::from_arr([1.0, -1.0])*cos(3.0*self.time);
-        rad *= 120.0/(rad[0]*rad[1]).sqrt();
-        self.canvas.draw(
-            &Path::Ellipse {
-                pos: Vec2f64::from_arr([400.0, 300.0]),
-                rad, //: Vec2f64::from_arr([160.0, 100.0*(2.0 + cos(self.time))]),
-                rot: 2.0*PI*self.time/19.0,
-                angle: Vec2f64::from_arr([0.0, 2.0*PI]),
-            },
-            &Method::Fill { color: Color::from_arr([0.5, 0.2, 1.0, 1.0]) },
-        );
+        self.canvas.fill(Color::from_arr([1.0,1.0,1.0,0.01]));
+        for body in &self.system.bodies {
+            self.canvas.draw(
+                &Path::Circle {
+                    pos: body.var.pos,
+                    rad: body.mass,
+                },
+                &Method::Fill { color: body.color },
+            );
+        }
     }
 }
--- src/physics.rs
+++ src/physics.rs
@@ -1,12 +1,40 @@
-use std::iter::{Iterator, Chain, IntoIterator, Flatten};
+use std::iter::{Iterator, Chain, IntoIterator, FlatMap};
 use vecmat::vec::*;
 
 pub trait Variable<'a> {
     type VarIter: Iterator<Item=&'a mut f64>;
+    type VarState: Variable<'a>;
     fn var_iter(&'a mut self) -> Self::VarIter;
+    fn var_clone(&'a self) -> Self::VarState;
 }
 
-pub fn solve_euler<'a, V> (y: &'a mut V, dy: &'a mut V, dt: f64) where V: Variable<'a> {
+pub fn solve_euler<'a, V, F> (
+    y: &'a mut V,
+    dy: &'a mut V::VarState,
+    dt: f64,
+    mut f: F,
+) where 
+    V: Variable<'a>, 
+    F: FnMut(&V, &mut V::VarState) 
+{
+    f(y, dy);
+    for (x, dx) in y.var_iter().zip(dy.var_iter()) {
+        *x += *dx*dt;
+    }
+}
+
+pub fn solve_rk4<'a, V, F> (
+    y: &'a mut V,
+    dy: &'a mut V::VarState,
+    _y1: &'a mut V::VarState,
+    _y2: &'a mut V::VarState,
+    dt: f64,
+    mut f: F,
+) where 
+    V: Variable<'a>, 
+    F: FnMut(&V, &mut V::VarState) 
+{
+    f(y, dy);
     for (x, dx) in y.var_iter().zip(dy.var_iter()) {
         *x += *dx*dt;
     }
@@ -14,82 +42,39 @@ pub fn solve_euler<'a, V> (y: &'a mut V, dy: &'a mut V, dt: f64) where V: Variab
 
 impl<'a> Variable<'a> for Vec2f64 {
     type VarIter = <&'a mut Vec2f64 as IntoIterator>::IntoIter;
+    type VarState = Self;
     fn var_iter(&'a mut self) -> Self::VarIter {
         self.iter_mut()
+    }
+    fn var_clone(&'a self) -> Self::VarState {
+        self.clone()
     }
 }
 
 #[derive(Clone)]
 pub struct Point2 {
-    pos: Vec2f64,
-    vel: Vec2f64,
+    pub pos: Vec2f64,
+    pub vel: Vec2f64,
 }
 
 impl<'a> Variable<'a> for Point2 {
     type VarIter = Chain<<Vec2f64 as Variable<'a>>::VarIter, <Vec2f64 as Variable<'a>>::VarIter>;
+    type VarState = Self;
     fn var_iter(&'a mut self) -> Self::VarIter {
         self.pos.var_iter().chain(self.vel.var_iter())
     }
-}
-/*
-impl<'a, II> Variable<'a> for IntoIterator<Item=&'a mut Variable<'a>, IntoIter=II> {
-    type VarIter = FlatMap<>
-    fn var_iter(&'a mut self) -> Self::VarIter {
-
-    }
-}
-*/
-/*
-pub struct FlatVarIter<'a, O, V: 'a> where O: Iterator<Item=&'a mut V>, V: Variable<'a> {
-    outer: O,
-    var: Option<V::VarIter>,
-}
-
-impl<'a, O, V> FlatVarIter<'a, O, V> where O: Iterator<Item=&'a mut V>, V: Variable<'a> {
-    pub fn new(mut outer: O) -> Self {
-        let var = match outer.next() {
-            Some(v) => {
-                Some(v.var_iter())
-            },
-            None => None,
-        };
-        Self {
-            outer,
-            var,
-        }
-    }
-
-    fn step_outer(&mut self) {
-
+    fn var_clone(&'a self) -> Self::VarState {
+        self.clone()
     }
 }
 
-impl<'a, O, V: 'a> Iterator for FlatVarIter<'a, O, V> where O: Iterator<Item=&'a mut V>, V: Variable<'a> {
-    type Item = &'a mut f64;
-    fn next(&mut self) -> Option<Self::Item> {
-        match self.var {
-            Some(mut v) => {
-                match v.next() {
-                    Some(x) => Some(x),
-                    None => {
-                        self.var = match self.outer.next() {
-                            Some(v) => Some(v.var_iter()),
-                            None => None
-                        };
-                        self.next()
-                    }
-                }
-            },
-            None => None
-        }
-    }
-}
-*/
-/*
 impl<'a, V: 'a> Variable<'a> for Vec<V> where V: Variable<'a> {
-    type VarIter = VecVarIter<'a, V>;
-    fn var_iter(&mut self) -> Self::VarIter {
-
+    type VarIter = FlatMap<<&'a mut Self as IntoIterator>::IntoIter, V::VarIter, fn(&'a mut V) -> V::VarIter>;
+    type VarState = Vec<V::VarState>;
+    fn var_iter(&'a mut self) -> Self::VarIter {
+        self.into_iter().flat_map(|v| v.var_iter())
+    }
+    fn var_clone(&'a self) -> Self::VarState {
+        self.into_iter().map(|v| v.var_clone()).collect()
     }
 }
-*/
