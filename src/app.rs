6766be2db5f694497caaa308049806484c283f6e
Alex <alexey.gerasev@gmail.com>
2018-10-12 11:01:26 +0700

    implement modular structure

--- src/app.rs
+++ src/app.rs
@@ -79,6 +79,7 @@ impl App {
         for b in &mut self.system.bodies {
             b.step(&self.system.body_cfg, self.time);
         }
+        console::log("step");
     }
 
     fn render(&mut self) {
